729615c6413e2500ed09d27e73d31aba96366035
Brad <bradleyglaurie@gmail.com>
2024-02-13 15:39:45 +0000

    Disable card animation

--- src/components/GlowCard.svelte
+++ src/components/GlowCard.svelte
@@ -8,21 +8,22 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		width: 16px;
+		width: 65%;
 		max-width: 280px;
-		height: 0px;
+		height: 75%;
 		max-height: 450px;
 		background-color: #1d1d1d;
 		border: 2px solid #ffffff;
 		border-radius: 16px;
 		box-shadow: 0 0 8px #ffffff;
+		filter: drop-shadow(30px 15px 4px rgba(15, 15, 15, 0.75));
 		overflow: hidden;
 		animation: grow 2s ease-in-out forwards;
 		animation-delay: 0.5s;
 		text-align: center;
 	}
 
-	@keyframes grow {
+	/* @keyframes grow {
 		12.5% {
 			height: 0;
 		}
@@ -34,5 +35,5 @@
 			height: 75%;
 			filter: drop-shadow(30px 15px 4px rgba(15, 15, 15, 0.75));
 		}
-	}
+	} */
 </style>
