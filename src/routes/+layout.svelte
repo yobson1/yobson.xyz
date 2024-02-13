9a6469c5840c93ff6c902c9be7efd645bc9622b0
Brad <bradleyglaurie@gmail.com>
2024-02-13 02:43:48 +0000

    Move shader background to layout and add global CSS file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../app.css';
+
+	import ShaderRenderer from '../components/ShaderRenderer.svelte';
+	import vertShader from '../shaders/isovalues/vert.glsl?raw';
+	import fragShader from '../shaders/isovalues/frag.glsl?raw';
+</script>
+
+<ShaderRenderer {vertShader} {fragShader}>
+	<div slot="shader-container" class="background-shader" />
+</ShaderRenderer>
+
+<slot />
+
+<style>
+	.background-shader {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: -1;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,5 @@
 <script>
-	import ShaderRenderer from '../components/ShaderRenderer.svelte';
-	import vertShader from '../shaders/isovalues/vert.glsl?raw';
-	import fragShader from '../shaders/isovalues/frag.glsl?raw';
+	import GlowCard from '../components/GlowCard.svelte';
 </script>
 
-<ShaderRenderer {vertShader} {fragShader}>
-	<div slot="shader-container" class="background-shader" />
-</ShaderRenderer>
-
-<style>
-	.background-shader {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
-</style>
+<GlowCard name="yobson" />
