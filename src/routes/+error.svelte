84716f1a4fcf7c2d5f6192c07f6f7d5624c1fcb4
Brad <bradleyglaurie@gmail.com>
2024-05-09 20:00:50 +0100

    Add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	onMount(() => {
+		if ($page.status == 404) {
+			window.location.href = '/';
+		}
+	});
+</script>
+
+<div>
+	<h1>{$page.status}</h1>
+	<h2>{$page.error?.message}</h2>
+</div>
+
+<style>
+	div {
+		inset: 0;
+		margin: auto;
+		position: fixed;
+		width: fit-content;
+		height: fit-content;
+	}
+
+	h1,
+	h2 {
+		margin: 4px 0;
+		color: #f8f8f8;
+		text-align: center;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+</style>
